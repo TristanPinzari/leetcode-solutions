b6eb44a58354a5f72fa965537cb0423c6a91298b
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-29 17:11:45 -0400

    Time: 53 ms (74.08%), Space: 59.4 MB (60.44%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -1,14 +1,10 @@
 function removeDuplicates(nums: number[]): number {
-    let index = 0;
-    let curr = null;
-    for (let i = 0; i < nums.length; i++) {
-        if (curr !== nums[i]) {
-            nums[index++] = nums[i];
-            if (nums[i] !== nums[i + 1]) continue;
-            nums[index++] = nums[i];
-            curr = nums[i];
-            i++
+    let i = 0;
+    for (const ele of nums) {
+        if (i === 0 || i === 1 || nums[i - 2] !== ele) {
+            nums[i] = ele;
+            i++;
         }
     }
-    return index;
+    return i;
 }
