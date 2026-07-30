5944d9a362cc9839cb0ef1f7c16f4f2e50d6bd26
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-30 16:07:40 -0400

    Time: 1 ms (85.29%), Space: 66.2 MB (53.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -2,11 +2,15 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    const originalLength = nums.length;
-    const shiftTimes = k % originalLength;
+    const n = nums.length;
+    const shiftTimes = k % n;
     if (shiftTimes === 0) return;
-    for (let i = 0; i < originalLength; i++) {
-        nums.push(nums[(originalLength + i - shiftTimes) % originalLength])
+
+    const rotated = new Array(n);
+    for (let i = 0; i < n; i++) {
+        rotated[(i + shiftTimes) % n] = nums[i];
     }
-    nums.splice(0, originalLength)
-};
+    for (let i = 0; i < n; i++) {
+        nums[i] = rotated[i];
+    }
+}
