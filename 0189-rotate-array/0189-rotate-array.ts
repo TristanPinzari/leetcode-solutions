a0856ebc7527bf08fcaf8c1a4f2a89cc1c3a1376
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-30 15:38:45 -0400

    Time: 3 ms (48.15%), Space: 66.2 MB (57.35%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -3,8 +3,10 @@
  */
 function rotate(nums: number[], k: number): void {
     const originalLength = nums.length;
-    for (let i = 0; i < k % originalLength; i++) {
-        nums.splice(0, 0, nums[originalLength - 1])
-        nums.length = originalLength;
+    const shiftTimes = k % originalLength;
+    if (shiftTimes === 0) return;
+    for (let i = 0; i < originalLength; i++) {
+        nums.push(nums[(originalLength + i - shiftTimes) % originalLength])
     }
+    nums.splice(0, originalLength)
 };
