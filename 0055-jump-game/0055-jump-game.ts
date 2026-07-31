46d1c57fa5fab9236d6059dfb182c801518a6f5d
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-31 13:22:50 -0400

    Time: 1 ms (86.22%), Space: 59.7 MB (61.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.ts
+++ 0055-jump-game/0055-jump-game.ts
@@ -1,12 +1,7 @@
 function canJump(nums: number[]): boolean {
     let lastStep = nums.length - 1;
-    while (lastStep !== 0) {
-        let nextStep = lastStep;
-        for (let i = lastStep - 1; i >= 0; i--) {
-            if (nums[i] >= lastStep - i) nextStep = i;
-        }
-        if (nextStep === lastStep) return false;
-        lastStep = nextStep; 
+    for (let i = lastStep - 1; i >= 0; i--) {
+        if (nums[i] >= lastStep - i) lastStep = i;
     }
-    return true;
+    return lastStep === 0;
 };
