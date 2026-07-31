6365075447098fa51909db5d47b8e9d1aa7e5563
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-31 15:46:08 -0400

    Time: 71 ms (87.32%), Space: 108.4 MB (46.89%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.ts
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.ts
@@ -1,29 +1,49 @@
 class RandomizedSet {
-    set;
-    constructor() {
-        this.set = new Set();
-    }
+    
+    private _container = [];
+    private _hashTable: Array<number> = []; //stores the index into _container
+    
+    constructor() {}
 
     insert(val: number): boolean {
-        const output = !this.set.has(val);
-        this.set.add(val);
-        return output;
+        if(this._hashTable[val] !== undefined) {
+            return false;
+        }
+        this._container.push(val);
+        this._hashTable[val] = this._container.length - 1;
+        return true;
     }
 
     remove(val: number): boolean {
-        const output = this.set.has(val);
-        this.set.delete(val);
-        return output;
+        // console.log(`Container Before removing ${val}: ${this._container}`)
+        const index = this._hashTable[val];
+        if(index === undefined) {
+            return false;
+        }
+
+        else if(this._container.length > 1 && index !== this._container.length - 1) {
+            const lastVal = this._container.pop();
+            this._hashTable[val] = undefined;
+            this._container[index] = lastVal;
+            this._hashTable[lastVal] = index;
+        }
+        else {
+            const lastVal = this._container.pop();
+            this._hashTable[val] = undefined;
+        }
+        
+        // console.log(`Container After removing ${val}: ${this._container}`);
+        return true;
     }
 
     getRandom(): number {
-        return [...this.set][Math.floor(Math.random() * this.set.size)];
+        return this._container[Math.floor(Math.random() * this._container.length)];
     }
 }
 
 /**
- * Your Randomizedthis.set object will be instantiated and called as such:
- * var obj = new Randomizedthis.set()
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
  * var param_1 = obj.insert(val)
  * var param_2 = obj.remove(val)
  * var param_3 = obj.getRandom()
