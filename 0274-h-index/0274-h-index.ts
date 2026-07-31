3892e72a4b0c4e257ee88c65bf391a58aa10086d
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-31 15:19:01 -0400

    Time: 352 ms (5.21%), Space: 61.8 MB (5.21%) - LeetHub

--- 0274-h-index/0274-h-index.ts
+++ 0274-h-index/0274-h-index.ts
@@ -1,10 +1,11 @@
 function hIndex(citations: number[]): number {
-    let output1, output2;
-    for (const num of citations.sort((a, b) => b - a)) {
+    let output1 = 0, output2 = 0;
+    for (const num of citations.sort((a, b) => a - b)) {
         if (citations.filter(n => n >= num).length >= num) {
-            output1 = num;
-            break;
+            if (num > output1) output1 = num;
+            continue;
         };
+        break;
     }
     for (let i = citations.length; i > 0; i--) {
         if (citations.filter(n => n >= i).length >= i) {
@@ -12,5 +13,5 @@ function hIndex(citations: number[]): number {
             break;
         }
     }
-    return Math.max(output1 || 0, output2 || 0);
+    return Math.max(output1, output2);
 };
