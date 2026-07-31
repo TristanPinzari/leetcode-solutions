f3b19d82e9388a70008642484fe23ac5f6a7a9c0
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-07-31 14:25:45 -0400

    Time: 1 ms (82.44%), Space: 55.9 MB (91.5%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,14 +1,15 @@
 function jump(nums: number[]): number {
-    if (nums.length === 1) return 0;
-    let lastStep = 0, jumps = 0;
-    while (lastStep !== nums.length - 1) {
-        if (lastStep + nums[lastStep] >= nums.length - 1) return jumps + 1;
-        let furthest = 0, newPos = 0;
-        for (let i = 1; i <= nums[lastStep]; i++) {
-            const pos = lastStep + i + nums[lastStep + i];
-            if (pos > furthest) furthest = pos, newPos = lastStep + i;
+    let near = 0, far = 0, jumps = 0;
+
+    while (far < nums.length - 1) {
+        let farthest = 0;
+        for (let i = near; i <= far; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
         }
-        lastStep = newPos;
+        near = far + 1;
+        far = farthest;
         jumps++;
     }
+
+    return jumps;    
 };
