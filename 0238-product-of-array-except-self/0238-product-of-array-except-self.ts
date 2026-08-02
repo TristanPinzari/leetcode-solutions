45af3a45dd28b4e97df7fab6d627dce74917961a
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-02 16:10:08 -0400

    Time: 4 ms (86.9%), Space: 67.8 MB (97.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,16 +1,18 @@
 function productExceptSelf(nums: number[]): number[] {
-    let temp = new Array(nums.length);
-    let answer = new Array(nums.length);
-    for (let i = 0; i < nums.length; i++) {
-        temp[i] = nums[i] * (temp[i - 1] ?? 1);
+let n = nums.length;
+    let res = new Array(n);
+
+    let pre = 1;
+    for(let i = 0; i < n; i++){
+        res[i] = pre;
+        pre *= nums[i];
     }
-    console.log(temp);
-    for (let i = nums.length - 2; i >= 0; i--) {
-        nums[i] = nums[i] * nums[i + 1];
+
+    let suf = 1;
+    for(let i = n - 1; i >= 0; i--){
+        res[i] *= suf;
+        suf *= nums[i];
     }
-    console.log(nums);
-    for (let i = 0; i < nums.length; i++) {
-        answer[i] = (temp[i - 1] ?? 1) * (nums[i + 1] ?? 1);
-    }
-    return answer;
+
+    return res;
 };
