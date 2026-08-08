db5e2eedf47e67c526a91ace4daa4a0d66a28560
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-08 15:38:15 -0400

    Time: 3 ms (39.09%), Space: 57.5 MB (59.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,3 +1,12 @@
 function reverseWords(s: string): string {
-    return s.split(" ").map((value) => value.trim()).filter(Boolean).reverse().join(" ");
+    let words = s.split(' ');
+    let res = [];
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i]) {
+            res.push(words[i]);
+        }
+    }
+
+    return res.join(' ');
 };
