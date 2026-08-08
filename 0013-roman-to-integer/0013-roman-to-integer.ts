6aa6b5e7ead842c1e5b0f0baf5ee133abce6952c
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-08 11:59:25 -0400

    Time: 3 ms (91.01%), Space: 60.9 MB (90.93%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.ts
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -1,27 +1,22 @@
 function romanToInt(s: string): number {
-    let total = 0;
-    const valueMap = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000,
-    }
+    let res = 0;
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
-    const arr = new Array(s.length);
-    for (let i = 0; i < s.length; i++) {
-        arr[i] = valueMap[s[i]];
-    }
-
-    for (let i = 0; i < s.length; i++) {
-        if (arr[i] < arr[i + 1]) {
-            arr[i + 1] -= arr[i];
+    for (let i = 0; i < s.length - 1; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            res -= roman[s[i]];
         } else {
-            total += arr[i];
-        };
+            res += roman[s[i]];
+        }
     }
 
-    return total;
+    return res + roman[s[s.length - 1]];    
 };
