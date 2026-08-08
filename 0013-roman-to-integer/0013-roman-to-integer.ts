8bcde496671abd5362180157902ac1f9b3faaf4f
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-08 11:56:36 -0400

    Time: 95 ms (5.08%), Space: 66.4 MB (5.01%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,28 @@
+function romanToInt(s: string): number {
+    let total = 0;
+    const valueMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    }
+
+    const arr = new Array(s.length);
+    for (let i = 0; i < s.length; i++) {
+        arr[i] = valueMap[s[i]];
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (arr[i] < arr[i + 1]) {
+            arr[i + 1] -= arr[i];
+        } else {
+            total += arr[i];
+        };
+    }
+
+    console.log(arr);
+    return total;
+};
