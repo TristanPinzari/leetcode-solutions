d950bb1654b7b1daffd7df519772c6910d336ed3
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-09 15:38:54 -0400

    Time: 29 ms (90.56%), Space: 73.6 MB (45.82%) - LeetHub

--- 0015-3sum/0015-3sum.ts
+++ 0015-3sum/0015-3sum.ts
@@ -1,25 +1,31 @@
 function threeSum(nums: number[]): number[][] {
-    const result = [];
+    let res = [];
     nums.sort((a, b) => a - b);
 
     for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        if (i > 0 && nums[i] === nums[i-1]) {
+            continue;
+        }
+        
+        let j = i + 1;
+        let k = nums.length - 1;
 
-        let j = i + 1, k = nums.length - 1;
         while (j < k) {
-            const sum = nums[i] + nums[j] + nums[k];
-            if (sum === 0) {
-                result.push([nums[i], nums[j], nums[k]]);
-                j++;
+            let total = nums[i] + nums[j] + nums[k];
+
+            if (total > 0) {
                 k--;
-                while (j < k && nums[j] === nums[j - 1]) j++;
-                while (j < k && nums[k] === nums[k + 1]) k--;
-            } else if (sum < 0) {
+            } else if (total < 0) {
                 j++;
             } else {
-                k--;
+                res.push([nums[i], nums[j], nums[k]]);
+                j++;
+
+                while (nums[j] === nums[j-1] && j < k) {
+                    j++;
+                }
             }
         }
     }
-    return result;
+    return res;    
 };
