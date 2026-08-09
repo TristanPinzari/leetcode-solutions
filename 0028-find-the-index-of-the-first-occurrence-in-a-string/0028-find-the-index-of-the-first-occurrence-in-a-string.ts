60fae403e2ac3a5af1d321981859bad07fea1937
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-09 11:01:04 -0400

    Time: 0 ms (100%), Space: 55.3 MB (69.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,3 +1,10 @@
 function strStr(haystack: string, needle: string): number {
-    return haystack.indexOf(needle);
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let bool = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (needle[j] !== haystack[i + j]) bool = false;
+        }
+        if (bool) return i;
+    }
+    return -1;
 };
