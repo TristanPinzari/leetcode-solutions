22517fb6a3b6930730be22bd48cbbbfe4b9ebee9
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-10 18:08:19 -0400

    Time: 0 ms (100%), Space: 61.2 MB (16.47%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
@@ -1,38 +1,18 @@
 function minSubArrayLen(target: number, nums: number[]): number {
-    let lowest = null, total = nums[0], left = 0, right = 0;
     const n = nums.length;
-    while (true) {
-        if (total > target) {
-            lowest = lowest ? Math.min(lowest, right - left + 1) : right - left + 1;
-            const delta = right - left;
-            if (delta <= 1) {
-                if (delta === 0) {
-                    if (right + 1 >= nums.length) return lowest ?? 0;
-                    left++;
-                    right++;
-                    total = nums[right];
-                } else {
-                    const canShiftRight = right + 1 < nums.length;
-                    if (canShiftRight) right++;
-                    total += canShiftRight ? nums[right] : 0;
-                    total -= nums[left];
-                    left++;
-                }
-            } else {
-                total -= nums[left];
-                left++;
-            }
-        } else if (total < target) {
-            if (right + 1 >= nums.length) return lowest ?? 0;
-            right++;
-            total += nums[right];
-        } else {
-            lowest = lowest ? Math.min(lowest, right - left + 1) : right - left + 1;
-            if (right + 1 >= nums.length) return lowest ?? 0;
-            right++;
-            total += nums[right];
+    let left = 0;
+    let total = 0;
+    let lowest = Infinity;
+
+    for (let right = 0; right < n; right++) {
+        total += nums[right];
+
+        while (total >= target) {
+            lowest = Math.min(lowest, right - left + 1);
+            total -= nums[left];
+            left++;
         }
-        console.log(left, right, total);
     }
-    return lowest ?? 0;
-};
+
+    return lowest === Infinity ? 0 : lowest;
+}
