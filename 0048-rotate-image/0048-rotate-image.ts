3f4b92de1771ed81d34394e8588264948fea8360
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-14 13:43:53 -0400

    Time: 1 ms (23.1%), Space: 56.3 MB (54.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.ts
+++ 0048-rotate-image/0048-rotate-image.ts
@@ -2,8 +2,8 @@
  Do not return anything, modify matrix in-place instead.
  */
 function rotate(matrix: number[][]): void {
-    const n = matrix.length, layers = Math.floor(n / 2);
-    for (let i = 0; i < layers; i++) {
+    const n = matrix.length;
+    for (let i = 0; i < Math.floor(n / 2); i++) {
         for (let j = i; j < i + n - i * 2 - 1; j++) {
             let sorted = 0, last = [i, j, matrix[i][j]];
             while (sorted !== 4) {
