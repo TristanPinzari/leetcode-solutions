175d3d11a00aaab6f08de5e160148c98d3d7dcb6
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-16 17:42:15 -0400

    Time: 12 ms (24.36%), Space: 60.5 MB (77.65%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -0,0 +1,27 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+    const mSet = new Set<number>(), nSet = new Set<number>(), m = matrix.length, n = matrix[0].length;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                mSet.add(i);
+                nSet.add(j);
+            };
+        }
+    }
+
+    for (const mCurr of mSet) {
+         for (let i = 0; i < n; i++) {
+            matrix[mCurr][i] = 0;
+        }
+    }
+
+    for (const nCurr of nSet) {
+         for (let i = 0; i < m; i++) {
+            matrix[i][nCurr] = 0;
+        }
+    }
+};
