15445ca01b4e760a0c0f3c99cc480b3fbb4bc01c
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-18 18:43:47 -0400

    Time: 110 ms (10.56%), Space: 70.3 MB (10.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -1,25 +1,37 @@
-function numIslands(grid: string[][]): number {
-    const m = grid[0].length, n = grid.length;
-    let island = 0;
+var numIslands = function(grid) {
+    let islands = 0;
+    const visited = new Set();
+    const rows = grid.length;
+    const cols = grid[0].length;
 
-    function destroyIsland(i, j) {
-        if (grid[i] && grid[i][j] && grid[i][j] === "1") {
-            grid[i][j] = "0";
-            destroyIsland(i, j + 1);
-            destroyIsland(i, j - 1);
-            destroyIsland(i + 1, j);
-            destroyIsland(i - 1, j);
+    const bfs = (r, c) => {
+        const q = [];
+        visited.add(`${r},${c}`);
+        q.push([r, c]);
+
+        while (q.length > 0) {
+            const [row, col] = q.shift();
+            const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+            for (const [dr, dc] of directions) {
+                const nr = row + dr;
+                const nc = col + dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === "1" && !visited.has(`${nr},${nc}`)) {
+                    q.push([nr, nc]);
+                    visited.add(`${nr},${nc}`);
+                }
+            }
         }
-    }
+    };
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (grid[i][j] === "1") {
-                destroyIsland(i, j);
-                island++;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === "1" && !visited.has(`${r},${c}`)) {
+                islands += 1;
+                bfs(r, c);
             }
         }
     }
 
-    return island;
+    return islands;    
 };
