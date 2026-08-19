299cc74dc84d17fb5251bff7f3a127062284dedf
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-19 16:15:59 -0400

    Time: 5 ms (21.62%), Space: 54.2 MB (95.5%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.ts
+++ 0289-game-of-life/0289-game-of-life.ts
@@ -4,22 +4,16 @@
 
 function gameOfLife(board: number[][]): void {
     const copy = board.map(row => [...row]);
-    
-    function liveNeighbors(row, col) {
-        let live = 0;
-        for (let dr = -1; dr <= 1; dr++) {
-            for (let dc = -1; dc <= 1; dc++) {
-                if (dr === 0 && dc === 0) continue; // skip the cell itself
-                const r = row + dr, c = col + dc;
-                if (copy[r] && copy[r][c] && copy[r][c] === 1) live++;
-            }
-        }
-        return live;
-    }
-
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[0].length; col++) {
-            const live = liveNeighbors(row, col);
+            let live = 0;
+            for (let dr = -1; dr <= 1; dr++) {
+                for (let dc = -1; dc <= 1; dc++) {
+                    if (dr === 0 && dc === 0) continue; // skip the cell itself
+                    const r = row + dr, c = col + dc;
+                    if (copy[r] && copy[r][c] && copy[r][c] === 1) live++;
+                }
+            }
             if (live < 2 || live > 3) {
                 board[row][col] = 0;
             } else if (live === 3) {
