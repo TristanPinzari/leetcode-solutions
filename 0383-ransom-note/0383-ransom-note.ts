26c14c3adaf01777b46ed259dfc8cccf5b393633
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-20 14:37:34 -0400

    Time: 18 ms (50.35%), Space: 58.9 MB (67.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.ts
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -1,15 +1,16 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
     if (ransomNote.length > magazine.length) return false;
-    const ransomMap = new Map(), magazineMap = new Map();
-    for (let i = 0; i < magazine.length; i++) {
-        const ransomeCurr = ransomNote[i], magazineCurr = magazine[i];
-        if (ransomNote[i]) ransomMap.set(ransomeCurr, (ransomMap.get(ransomeCurr) ?? 0) + 1);
-        magazineMap.set(magazineCurr, (magazineMap.get(magazineCurr) ?? 0) + 1);
+
+    const magazineMap = new Map<string, number>();
+    for (const char of magazine) {
+        magazineMap.set(char, (magazineMap.get(char) ?? 0) + 1);
     }
 
-    for (const [char, count] of ransomMap) {
-        if ((magazineMap.get(char) ?? 0) < count) return false;
+    for (const char of ransomNote) {
+        const count = magazineMap.get(char) ?? 0;
+        if (count === 0) return false;
+        magazineMap.set(char, count - 1);
     }
-    
+
     return true;
-};
+}
