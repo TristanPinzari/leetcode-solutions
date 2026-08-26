80b881ceb818cc0df39a4a58152b18ca8dffa97e
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-26 16:49:48 -0400

    Time: 14 ms (99.26%), Space: 69.5 MB (26.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,27 +1,17 @@
-function indexString(s: string) {
-    const minCharCode  = 97;
-    const arr = new Array(26).fill(0)
-    
-    for(let i = 0; i < s.length; i++) {
-        const charCode = s[i].charCodeAt(0) - minCharCode
-        arr[charCode] += 1;
-    }
-
-    return arr.toString();
-}
-
 function groupAnagrams(strs: string[]): string[][] {
-    const map = new Map(), res = new Array();
-    for (const str of strs) {
-        const arrStr = indexString(str);
-        if (map.has(arrStr)) {
-            map.get(arrStr).push(str);
-        } else {
-            map.set(arrStr, [str]);
+    const groupHashMap = new Map<string, Array<string>>();
+
+    for(let i = 0; i < strs.length; i ++) {
+        const stringKey = new Array(26).fill(0);
+        
+        for(let j = 0; j < strs[i].length; j ++) {
+            stringKey[strs[i].charCodeAt(j) - 97]++;
         }
+        const key = String.fromCharCode(...stringKey);
+        const bucket = groupHashMap.get(key) || [];
+        bucket.push(strs[i]);
+        groupHashMap.set(key, bucket);
     }
-    for (const [key, val] of map) {
-        res.push(val);
-    }
-    return res;
-};
+
+    return [...groupHashMap.values()];
+}
