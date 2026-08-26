6caf335c1bdf16900a42a4ebb66b0245a5de451e
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-26 15:59:06 -0400

    Time: 0 ms (100%), Space: 55.8 MB (12.3%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.ts
+++ 0290-word-pattern/0290-word-pattern.ts
@@ -3,11 +3,8 @@ function wordPattern(pattern: string, s: string): boolean {
     if (pattern.length !== words.length) return false;
     for (let i = 0; i < words.length; i++) {
         const currWord = words[i], currPattern = pattern[i], mapVal = map.get(currWord);
-        console.log(currWord, currPattern, mapVal)
         if (mapVal && mapVal !== currPattern) return false;
-        console.log(1)
         if (!mapVal && set.has(currPattern)) return false;
-        console.log(2)
         map.set(currWord, currPattern);
         set.add(currPattern);
     }
