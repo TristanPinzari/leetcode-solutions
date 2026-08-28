1e98d9e0ceab040eec26cf5e863682cadc7c9bd8
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-28 16:20:31 -0400

    Time: 5 ms (13.03%), Space: 57.4 MB (58.72%) - LeetHub

--- 0202-happy-number/0202-happy-number.ts
+++ 0202-happy-number/0202-happy-number.ts
@@ -1,19 +1,20 @@
-function doSomething(n: number, set: Set<Number>): boolean {
-    let result = 0;
-    for (const digit of String(n)) {
-        result += Number(digit) ** 2;
-    }
-    if (result === 1) {
-        return true;
-    } else if (set.has(result)) {
-        return false;
-    } else {
-        set.add(result);
-        return doSomething(result, set);
-    }
-}
-
 function isHappy(n: number): boolean {
-    const set = new Set<Number>();
-    return doSomething(n, set);
-};
+    const set = new Set<number>();
+    
+    function doSomething(n: number): boolean {
+        let result = 0;
+        for (const digit of String(n)) {
+            result += Number(digit) ** 2;
+        }
+        if (result === 1) {
+            return true;
+        } else if (set.has(result)) {
+            return false;
+        } else {
+            set.add(result);
+            return doSomething(result);
+        }
+    }
+    
+    return doSomething(n);
+}
