81990c5c95408ec25b1ccead9d64bbbc6406b46d
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-28 14:38:24 -0400

    Time: 11 ms (35.86%), Space: 59.9 MB (5.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,18 +1,20 @@
 function twoSum(nums: number[], target: number): number[] {
     let left = 0, right = nums.length - 1;
-    const copy = [...nums].sort((a, b) => a - b);
+    const copy = [...nums].sort((a, b) => a - b), map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const curr  = nums[i], res = map.get(curr);
+        if  (res) {
+            res.push(i)
+        } else {
+            map.set(curr, [i]);
+        }
+    }
     
     while (left < right) {
         const sum = copy[left] + copy[right];
         if (sum === target) {
-            const first = nums.indexOf(copy[left]);
-            let second = nums.indexOf(copy[right]);
-            if (first === second) {
-                nums[first] = null;
-                second = nums.indexOf(copy[right]);
-                nums[first] = copy[left];
-            }
-            return[first, second];
+            return[map.get(copy[left])[0], map.get(copy[right])[1] ?? map.get(copy[right])[0]];
         }
         if  (sum < target) {
             left++;
