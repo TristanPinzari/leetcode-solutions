8116e97e8c7393db70dc784881fc444531990e75
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-08-29 14:55:18 -0400

    Time: 843 ms (5.01%), Space: 86.8 MB (7.78%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -0,0 +1,16 @@
+function longestConsecutive(nums: number[]): number {
+    if (nums.length === 0) return 0;
+    nums = Array.from(new Set(nums)).sort((a, b) => a - b);
+    console.log(nums)
+    let top = 1, count = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] - nums[i - 1] === 1) {
+            count++;
+        } else {
+            count = 1;
+        }
+        top = Math.max(top, count);
+        console.log(nums[i], nums[i - 1], count)
+    }
+    return top;
+};
