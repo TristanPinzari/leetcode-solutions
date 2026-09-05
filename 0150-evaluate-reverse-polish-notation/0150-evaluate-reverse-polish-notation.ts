741c81ac36aeb41b3b8cec1962e18d322743d304
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-09-05 16:17:20 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
@@ -1,8 +1,6 @@
 function evalRPN(tokens: string[]): number {
     const stack = new Array();
-    
     for (const token of tokens) {
-        console.log(stack)
         if (Number(token) || token === "0") {
             stack.push(Number(token));
             continue;
@@ -25,6 +23,5 @@ function evalRPN(tokens: string[]): number {
         }
         stack.push(res);
     }
-    console.log(stack)
     return stack[0];
 };
