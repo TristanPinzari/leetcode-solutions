e50e799ce306540bedd543fa4b8d0afbc91981b9
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-09-07 14:18:58 -0400

    Time: 2926 ms (5%), Space: 68.7 MB (18.75%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.ts
+++ 0224-basic-calculator/0224-basic-calculator.ts
@@ -1,33 +1,55 @@
-function calculate(s: string): number {
-    const stack: number[] = [];
-    let res = 0;
-    let curr = 0;
-    let sign = 1;
-
-    for (const c of s) {
-        if (/\d/.test(c)) {
-            curr = curr * 10 + Number(c);
-        } else if (c === '+') {
-            res += sign * curr;
-            sign = 1;
-            curr = 0;
-        } else if (c === '-') {
-            res += sign * curr;
-            sign = -1;
-            curr = 0;
-        } else if (c === '(') {
-            stack.push(res);
-            stack.push(sign);
-            res = 0;
-            sign = 1;
-        } else if (c === ')') {
-            res += sign * curr;
-            curr = 0;
-            res *= stack.pop()!;
-            res += stack.pop()!;
+function findCorrBracket(s: string, i: number): number {
+    let skip = 0;
+    for (let j = i + 1; j < s.length; j++) {
+        const char = s[j];
+        if (char === "(") {
+            skip++;
+            continue;
+        }
+        if (char === ")") {
+            if (skip > 0) {
+                skip--;
+                continue;
+            }
+            return j;
         }
     }
-
-    res += sign * curr;
-    return res;
 }
+
+function calculateNormalized(s: string): number {
+    let result = 0, left = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i - 1] === "-" && s[i] === "-") {
+            left = i + 1;
+            continue;
+        }
+        if (i === s.length - 1) {
+            result += Number(s.slice(left, i + 1))
+            return result;
+        };
+        if (left !== i && (s[i] === "+" || s[i] === "-")) {
+            result += Number(s.slice(left, i));
+            left = s[i] === "+" ? i + 1 : i;
+        }
+    }
+}
+
+function resolve(s: string): number {
+    let normalized = "";
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char === " " || char === ")") continue;
+        if (char === "(") {
+            const corrBracket = findCorrBracket(s, i);
+            normalized += resolve(s.slice(i + 1, corrBracket));
+            i = corrBracket;
+        } else {
+            normalized += char;
+        }
+    }
+    return calculateNormalized(normalized);
+}
+
+function calculate(s: string): number {
+    return resolve(s);
+};
