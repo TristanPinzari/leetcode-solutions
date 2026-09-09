302d94c40e94236f029f5b14426ec919e30db12a
Tristan Pinzari <138397262+TristanPinzari@users.noreply.github.com>
2026-09-09 11:01:06 -0400

    Time: 5 ms (33.66%), Space: 61.9 MB (66.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -1,25 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    let carry = 0;
-    let result: ListNode | null = null;
-    let tail: ListNode | null = null;
-
-    while (l1 || l2 || carry) {
-        const sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
-        carry = Math.floor(sum / 10);
-        const digit = sum % 10;
-
-        const newNode = new ListNode(digit);
-        if (!result) {
-            result = newNode;
-            tail = newNode;
-        } else {
-            tail!.next = newNode;
-            tail = newNode;
-        }
-
-        l1 = l1?.next ?? null;
-        l2 = l2?.next ?? null;
+    let carry = 0, first = null, node = null;
+    while (l1 || l2 || carry > 0) {
+        const l1Val = l1 ? l1.val : 0, l2Val = l2 ? l2.val : 0, newVal = carry + l1Val + l2Val;
+        const newNode = new ListNode(newVal % 10, null);
+        carry = Math.floor(newVal / 10);
+        if (!first) first = newNode;
+        if (node) node.next = newNode;
+        node = newNode;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
     }
-
-    return result;
-}
+    return first;
+};
